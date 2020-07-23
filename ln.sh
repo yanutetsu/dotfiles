2fd7f141c3819c5a799f0ee50b6d720555af9264
yanutetsu <satoshi.yanuma@topgate.co.jp>
2020-07-23 17:58:24 +0900

    .zshrcを対象に含める

--- ln.sh
+++ ln.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 ln -s $HOME/dotfiles/.bash_profile $HOME
 ln -s $HOME/dotfiles/.bashrc $HOME
+ln -s $HOME/dotfiles/.zshrc $HOME
 ln -s $HOME/dotfiles/.gitconfig $HOME
 ln -s $HOME/dotfiles/init.el $HOME/.emacs.d
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
